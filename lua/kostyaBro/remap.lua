b1a1c0d4eabb0535fb166fbd9eb12456c6150f89
kostyaBro <indianor201203@gmail.com>
2023-01-09 15:30:52 +0500

    feat: move between panes using leader key

--- lua/kostyaBro/remap.lua
+++ lua/kostyaBro/remap.lua
@@ -36,3 +36,10 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Move between panes
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+
